459783f37ada54c85e61dd87d26739133426ce8c
Vladimir Timofeev <vovkasm@gmail.com>
2016-11-13 21:30:31 +0300

    make android work with network sounds

--- android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
+++ android/src/main/java/com/zmxv/RNSound/RNSoundModule.java
@@ -2,6 +2,7 @@ package com.zmxv.RNSound;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaPlayer.OnErrorListener;
@@ -13,6 +14,7 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.common.MapBuilder;
 import com.facebook.react.module.annotations.ReactModule;
@@ -39,19 +41,41 @@ class RNSoundModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void prepare(final String fileName, final Integer key, final Callback callback) {
-        MediaPlayer player = createMediaPlayer(fileName);
-        if (player == null) {
-            WritableMap e = Arguments.createMap();
-            e.putInt("code", -1);
-            e.putString("message", "resource not found");
-            callback.invoke(e);
+    public void prepare(final ReadableMap source, final Integer key, final Callback callback) {
+        Log.d(TAG, "Got source " + source.toString());
+        final Uri uri = Uri.parse(source.getString("uri"));
+
+        MediaPlayer player = new MediaPlayer();
+        player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                Log.d(TAG, "onPrepared " + uri.toString());
+                WritableMap props = Arguments.createMap();
+                props.putDouble("duration", mp.getDuration() * .001);
+                callback.invoke(null, props);
+            }
+        });
+        player.setOnErrorListener(new OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                Log.e(TAG, "Got error " + what + " extra " + extra);
+                return false;
+            }
+        });
+
+        try {
+            player.setDataSource(uri.toString());
+        }
+        catch (Exception e) {
+            Log.e(TAG, "can't set data source: " + e.toString());
+            WritableMap jsError = Arguments.createMap();
+            jsError.putInt("code", -1);
+            jsError.putString("message", e.getMessage());
+            callback.invoke(jsError);
             return;
         }
         playerPool.put(key, player);
-        WritableMap props = Arguments.createMap();
-        props.putDouble("duration", player.getDuration() * .001);
-        callback.invoke(null, props);
+        player.prepareAsync();
     }
 
     private MediaPlayer createMediaPlayer(final String fileName) {
